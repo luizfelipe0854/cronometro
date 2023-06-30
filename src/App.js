e26e1ad2863523b9d6b80ec5401d6700edb65742
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2023-06-30 20:30:42 -0300

    Adicionando arquivos do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            numero: 0,
+            botao: "Iniciar"
+        }
+        this.timer = null;
+        this.iniciar = this.iniciar.bind(this);
+        this.limpar = this.limpar.bind(this);
+        
+        
+
+    }
+
+    iniciar(){
+        let state = this.state;
+
+        if(this.timer === null){
+            this.timer = setInterval(()=>{
+                state.numero += 0.1;
+                state.botao = "Pausar";
+                this.setState(state);
+            },100);           
+       } else{
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botao = "Iniciar";
+            this.setState(state);
+       }            
+    }
+
+    limpar(){
+        let state = this.state;
+        if(state.numero === 0){
+            alert("O cronômetro já está zerado!");
+       } else{
+            clearInterval(this.timer);
+            this.timer = null;
+            state.numero = 0;
+            this.setState(state);
+       }     
+
+    }
+    
+    render(){
+        return(
+            <div className="container">
+                <img src={require("./assets/cronometro.png")} alt="cronometro"/>
+                <a className="timer">{this.state.numero.toFixed(1)}</a>
+                <div className="areabotoes">
+                    <a className="botao" onClick={this.iniciar}>{this.state.botao}</a>
+                    <a className="botao" onClick={this.limpar}>Limpar</a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
